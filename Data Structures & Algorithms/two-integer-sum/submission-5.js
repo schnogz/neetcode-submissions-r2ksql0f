62b0ab79b4e138e0bdbc4aea4d0fbd9d79442d40
Andrew Schneider <andrew.mark.schneider@gmail.com>
2026-04-03 16:28:57 -0400

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.js
@@ -0,0 +1,68 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+
+    // brute force
+    // time - O(n^2)
+    // space - 0(1)
+    // twoSum(nums, target) {
+    //     for(let i = 0; i < nums.length; i++) {
+    //         for(let i2 = i + 1; i2 <= nums.length; i2++) {
+    //             if (nums[i] + nums[i2] === target) {
+    //                 return [i, i2]
+    //             }
+    //         }
+    //     }
+    // }
+
+    // sorting and pointers
+    // twoSum(nums, target) {
+    //     const mapped = nums
+    //         .map((x, i) => [x, i])
+    //         .sort((a, b) => a[0] - b[0]);
+
+    //     let l = 0;
+    //     let r = mapped.length - 1
+
+    //     while(l < r) {
+    //         let currentSum = mapped[l][0] + mapped[r][0];
+    //         if (currentSum === target) {
+    //             return [
+    //                 Math.min(mapped[l][1], mapped[r][1]), 
+    //                 Math.max(mapped[l][1], mapped[r][1]), 
+    //             ];
+    //         } else if (currentSum < target) {
+    //             l++;
+    //         } else {
+    //             r--;
+    //         }
+    //     }
+
+    //     return [];
+
+    // }
+
+
+    // hashMap
+    twoSum(nums, target) {
+        const vMap = new Map();
+
+        // num[1] + num[2] = target
+        // target 
+        
+        for(let i = 0; i < nums.length; i++) {
+            if (vMap.has(target - nums[i])) {
+                return [
+                    Math.min(vMap.get(target - nums[i]), i), 
+                    Math.max(vMap.get(target - nums[i]), i)
+                ]
+            }
+            vMap.set(nums[i], i)
+        }
+
+        return [];
+    }
+}
